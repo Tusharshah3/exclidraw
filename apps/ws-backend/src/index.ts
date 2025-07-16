000e2d54540a5479ccf89a7f7e07ad9d38a9ac0c
Tusharshah3 <tusharshah372003@gmail.com>
2025-07-16 17:30:17 +0530

    day first

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,24 @@
 import { WebSocketServer } from "ws";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
 const wss= new WebSocketServer({port:8080});
+import { JWT_SECRET } from "./config";
 
-wss.on('connection',function connection(ws){
+wss.on('connection',function connection(ws,request){
+    const url=request.url;
 
+    if(!url){return}
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token')||"";
+
+    const decoded=jwt.sign(token,JWT_SECRET);
+    if(typeof decoded==="string"){
+        return ;
+    }
+
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return;
+    }
     ws.on('message',function message(data){
         console.log('recieved data:%s',data);
         ws.send('hello from ws server');
