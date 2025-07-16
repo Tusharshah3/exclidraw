953b503c2a564c7f32386bc3a61ad0f1868a77c5
Tusharshah3 <tusharshah372003@gmail.com>
2025-07-16 20:09:36 +0530

    final for day 1

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,50 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types";
 
 const app= express();
+
+app.post("/signup",async(req,res)=>{
+    //db call
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"incorrect input"
+        })
+    }
+
+
+    res.json({userId:"12334"});
+
+})
+app.post("/signin",async(req,res)=>{
+
+    const data=SigninSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"incorrect input"
+        })
+    }
+
+    const userId = "12334"; // This should be fetched from the database after validating username and password
+    const token=JWT.sign({userId},JWT_SECRET);
+    res.status(200).json({"token":token});
+})
+
+app.post("/room",middleware,async(req,res)=>{
+    //dbcall 
+    const data=CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"incorrect input"
+        })
+    }
+
+    res.status(201).json({message: "Room created successfully"});
+})
+
 
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 const wss= new WebSocketServer({port:8080});
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 wss.on('connection',function connection(ws,request){
     const url=request.url;
@@ -12,6 +12,8 @@ wss.on('connection',function connection(ws,request){
 
     const decoded=jwt.sign(token,JWT_SECRET);
     if(typeof decoded==="string"){
+        ws.close();
+        console.error("invalide token in form of string");
         return ;
     }
 
